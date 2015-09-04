6831c4b3108e1c72fe20c51f1626f6122777cb3c
keerthas <keerthas@INkeerthas.local>
2015-09-04 18:00:37 +0530

    spec to check for valid input list after parsing

--- test/com/thoughtworks/baseline/InputParserTest.java
+++ test/com/thoughtworks/baseline/InputParserTest.java
@@ -14,4 +14,15 @@ public class InputParserTest {
         InputParser inputParser = new InputParser(items);
         assertEquals(items.size(), inputParser.parseInputList().size());
     }
+
+    @Test
+    public void shouldReturnValidItemList() {
+        ArrayList<String> items = new ArrayList<>();
+        items.add("1 book at 12.49");
+        ArrayList<Item> itemList = new ArrayList<Item>() {{
+            add(new Item("book", 1, 12.49, false));
+        }};
+        InputParser inputParser = new InputParser(items);
+        assertEquals(itemList, inputParser.parseInputList());
+    }
 }
